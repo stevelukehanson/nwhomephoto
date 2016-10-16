45297d942e60359fe420d85091649525a9ec8eef
stevelukehanson <stevelukehanson@gmail.com>
2016-10-16 12:38:02 -0700

    incremental path changes

--- devEnv.cmd
+++ devEnv.cmd
@@ -1,4 +1,4 @@
-set ANT_HOME=C:\apache\apache-ant-1.8.4
+set ANT_HOME=C:\apache\apache-ant-1.9.7
 set JAVA_HOME=C:\Program Files (x86)\Java\jre7
 //set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_21
 set FILEZILLA_HOME=C:\Program Files (x86)\Filezilla FTP Client
